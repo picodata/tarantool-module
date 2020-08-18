06ad54e46fc89b096093c11e193526c5ab363a6c
Anton Melnikov <volt0@picodata.io>
2020-08-18 19:23:05 +0300

    feat(error): error handling

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,23 +1,22 @@
 use std::io::Cursor;
 use std::os::raw::c_char;
 
-use rmp_serde::encode::Error as TupleEncodeError;
-use rmp_serde::decode::Error as TupleDecodeError;
 use serde::{de::DeserializeOwned, Serialize};
 
 use crate::c_api::{self, BoxTuple};
+use crate::error::ModuleError;
 
 pub struct Tuple {
     ptr: *mut BoxTuple,
 }
 
 impl Tuple {
-    pub fn new_from_struct<T>(value: &T) -> Result<Self, TupleEncodeError> where T: Serialize {
-        let format = unsafe { c_api::box_tuple_format_default() };
+    pub fn new_from_struct<T>(value: &T) -> Result<Self, ModuleError> where T: Serialize {
+        let format_ptr = unsafe { c_api::box_tuple_format_default() };
         let buf = rmp_serde::to_vec(value)?;
         let buf_ptr = buf.as_ptr() as *const c_char;
         let tuple_ptr = unsafe { c_api::box_tuple_new(
-            format,
+            format_ptr,
             buf_ptr,
             buf_ptr.offset(buf.len() as isize),
         ) };
@@ -27,6 +26,7 @@ impl Tuple {
     }
 
     pub(crate) fn from_ptr(ptr: *mut BoxTuple) -> Self {
+        unsafe { c_api::box_tuple_ref(ptr) };
         Tuple{ptr}
     }
 
@@ -38,21 +38,19 @@ impl Tuple {
         unsafe { c_api::box_tuple_bsize(self.ptr) }
     }
 
-    pub fn into_struct<T>(self) -> Result<T, TupleDecodeError> where T: DeserializeOwned {
+    pub fn into_struct<T>(self) -> Result<T, ModuleError> where T: DeserializeOwned {
         let raw_data_size = self.size();
         let mut raw_data = Vec::<u8>::with_capacity(raw_data_size);
+
         let actual_size = unsafe {
             c_api::box_tuple_to_buf(self.ptr, raw_data.as_ptr() as *mut c_char, raw_data_size)
         };
-
         if actual_size < 0 {
-            // TODO: correct handle error
-            panic!();
+            ModuleError::last()?;
         }
-        unsafe { raw_data.set_len(actual_size as usize) };
 
-        let reader = Cursor::new(raw_data);
-        Ok(rmp_serde::from_read::<_, T>(reader)?)
+        unsafe { raw_data.set_len(actual_size as usize) };
+        Ok(rmp_serde::from_read::<_, T>(Cursor::new(raw_data))?)
     }
 }
 
