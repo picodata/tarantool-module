7449b0a4064f84578d18cf2a46cd81cfec48a1f5
Anton Melnikov <volt0@picodata.io>
2020-12-25 10:31:50 +0300

    feat(benchmarks): bulk insert case (lua version)

---
+++ tests/src/bench_bulk_insert.lua
@@ -0,0 +1,82 @@
+#!/usr/bin/env tarantool
+
+box.cfg{
+    listen = 3301,
+    memtx_memory = 48 * 1024 * 1024 * 1024,
+    net_msg_max = 5000,
+    wal_mode = 'none'
+}
+
+-- Init test database
+box.once('bootstrap_bench', function()
+    box.schema.user.create('bench_user', { password = 'password' })
+    box.schema.user.grant('bench_user', 'read,write,execute,create,drop', 'universe')
+
+    local bench_s1 = box.schema.space.create('bench_s1')
+    bench_s1:format{
+        {name = 'id', type = 'unsigned'},
+        {name = 'text', type = 'string' }
+    }
+    bench_s1:create_index('primary', {type = 'TREE', parts = {{ field = 1, type = 'unsigned' }}})
+end)
+
+------------------------------------------------------------------------------
+
+clock = require('clock')
+fiber = require('fiber')
+net_box = require('net.box')
+
+local test_size = 1000;
+local num_fibers = 10;
+local num_rows = 1000;
+local num_passes = 100;
+
+local text = string.rep('X', test_size)
+
+local_space = box.space.bench_s1
+local_space:truncate()
+
+local pass_times = {}
+
+------------------------------------------------------------------------------
+
+for i = 1, num_passes do
+    local begin_time = tonumber(clock.monotonic64())
+
+    for i = 1, num_fibers do
+        fiber.create(
+            function(id_base)
+                local conn = net_box.connect('bench_user:password@127.0.0.1:3301')
+                local remote_space = conn.space.bench_s1
+                for id = 1, num_rows  do
+                    remote_space:insert{id + id_base, text}
+                end
+            end,
+            i * num_rows
+        )
+    end
+
+    local end_time = tonumber(clock.monotonic64())
+    table.insert(pass_times, end_time - begin_time)
+end
+
+------------------------------------------------------------------------------
+
+local avg = 0
+for i = 1, num_passes do
+    avg = avg + pass_times[i]
+end
+avg = avg / num_passes
+
+local std_div = 0
+for i = 1, num_passes do
+    local t = pass_times[i]
+    local dt = t - avg
+    std_div = std_div + (dt * dt)
+end
+std_div = math.sqrt(std_div / num_passes)
+
+print(math.floor(avg))
+print(math.floor(std_div))
+
+os.exit(0)
