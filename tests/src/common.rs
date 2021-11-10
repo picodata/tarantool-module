e2981540df8dacffbdd645cc9af98a856f3b3fd5
Georgy Moshkin <gmoshkin@picodata.io>
2021-11-10 14:08:37 +0300

    refactor(test): split fiber tests into multiple files

--- tests/src/common.rs
+++ tests/src/common.rs
@@ -38,3 +38,34 @@ pub struct QueryOperation {
 }
 
 impl AsTuple for QueryOperation {}
+
+#[derive(Clone, Debug)]
+pub(crate) struct DropCounter(pub(crate) std::rc::Rc<std::cell::Cell<usize>>);
+
+impl Drop for DropCounter {
+    fn drop(&mut self) {
+        let old_count = self.0.get();
+        self.0.set(old_count + 1);
+    }
+}
+
+pub(crate) fn capture_value<T>(_: &T) {}
+
+pub(crate) fn fiber_csw() -> i32 {
+    static mut FUNCTION_DEFINED: bool = false;
+    let mut lua = crate::hlua::global();
+
+    if unsafe { !FUNCTION_DEFINED } {
+        lua.execute::<()>(r#"
+        function fiber_csw()
+        local fiber = require('fiber')
+        return fiber.info()[fiber.id()].csw
+        end
+        "#).unwrap();
+        unsafe { FUNCTION_DEFINED = true; }
+    }
+
+    return lua.get::<tarantool::hlua::LuaFunction<_>, _>("fiber_csw")
+        .unwrap().call().unwrap();
+}
+
