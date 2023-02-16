32e279e25068e190a97cab4333e225a815b527a0
lowit <ea@lowit.ru>
2023-02-17 01:15:42 +0300

    datetime: remove unwraps

--- tarantool/src/datetime.rs
+++ tarantool/src/datetime.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::fmt::Display;
 use std::os::raw::c_char;
-use std::{convert::TryInto, fmt::Display};
 use time::{Duration, UtcOffset};
 
 type Inner = time::OffsetDateTime;
@@ -25,43 +26,48 @@ impl Datetime {
 
     /// Convert an array of bytes in the  endian order into a `DateTime`.
     #[inline(always)]
-    pub fn from_bytes(bytes: [u8; 16]) -> Self {
-        let sec_bytes: [u8; 8] = bytes[0..8].try_into().unwrap();
-        let nsec_bytes: [u8; 4] = bytes[8..12].try_into().unwrap();
-        let tzoffest_bytes: [u8; 2] = bytes[12..14].try_into().unwrap();
+    pub fn from_bytes(bytes: [u8; 16]) -> Result<Self, Box<dyn Error>> {
+        // pub fn from_bytes(bytes: [u8; 16]) -> Self {
+        let mut sec_bytes: [u8; 8] = [0; 8];
+        sec_bytes.copy_from_slice(&bytes[0..8]);
+        let mut nsec_bytes: [u8; 4] = [0; 4];
+        nsec_bytes.copy_from_slice(&bytes[8..12]);
+        let mut tzoffest_bytes: [u8; 2] = [0; 2];
+        tzoffest_bytes.copy_from_slice(&bytes[12..14]);
 
         let secs = i64::from_le_bytes(sec_bytes);
         let nsecs = u32::from_le_bytes(nsec_bytes);
         let tzoffset: i32 = i16::from_le_bytes(tzoffest_bytes).into();
 
-        let dt = Inner::from_unix_timestamp(secs)
-            .unwrap()
-            .to_offset(UtcOffset::from_whole_seconds(tzoffset * 60).unwrap())
+        let dt = Inner::from_unix_timestamp(secs)?
+            .to_offset(UtcOffset::from_whole_seconds(tzoffset * 60)?)
             + Duration::nanoseconds(nsecs as i64);
 
-        dt.into()
+        Ok(dt.into())
     }
 
     /// Convert a slice of bytes in the little endian order into a `DateTime`. Return
     /// `None` if there's not enough bytes in the slice.
     #[inline(always)]
     pub fn try_from_slice(bytes: &[u8]) -> Option<Self> {
-        std::convert::TryInto::try_into(bytes)
-            .ok()
-            .map(Self::from_bytes)
+        let b = std::convert::TryInto::try_into(bytes).ok();
+        if let Some(b) = b {
+            Self::from_bytes(b).ok()
+        } else {
+            None
+        }
     }
 
     /// Return an array of bytes in the little endian order
     #[inline(always)]
     pub fn as_bytes(&self) -> [u8; 16] {
-        let mut buf: Vec<u8> = vec![];
+        let mut buf: [u8; 16] = [0; 16];
 
-        buf.extend_from_slice(&self.inner.unix_timestamp().to_le_bytes());
-        buf.extend_from_slice(&self.inner.nanosecond().to_le_bytes());
-        buf.extend_from_slice(&self.inner.offset().whole_minutes().to_le_bytes());
-        buf.resize(16, 0);
+        buf[0..8].copy_from_slice(&self.inner.unix_timestamp().to_le_bytes());
+        buf[8..12].copy_from_slice(&self.inner.nanosecond().to_le_bytes());
+        buf[12..14].copy_from_slice(&self.inner.offset().whole_minutes().to_le_bytes());
 
-        buf.try_into().unwrap()
+        buf
     }
 }
 
@@ -137,9 +143,9 @@ mod tests {
     #[test]
     pub fn serialize() {
         let exp = [
-            216, 4, 176, 109, 109, 56, 0, 0, 0, 0, 0, 0, 0, 0, 76, 255, 0, 0,
+            216, 4, 23, 11, 79, 101, 0, 0, 0, 0, 208, 208, 28, 21, 76, 255, 0, 0,
         ];
-        let dt: Datetime = datetime!(2000-01-01 0:00 -3).into();
+        let dt: Datetime = datetime!(2023-11-11 2:03:19.35421 -3).into();
         let buf = vec![];
         let mut s = rmp_serde::encode::Serializer::new(buf);
         dt.serialize(&mut s).unwrap();
@@ -157,9 +163,9 @@ mod tests {
 
     #[test]
     pub fn deserialize() {
-        let exp: Datetime = datetime!(2000-01-01 0:00 -3).into();
+        let exp: Datetime = datetime!(2023-11-11 2:03:19.35421 -3).into();
         let data: &[u8] = &[
-            216, 4, 176, 109, 109, 56, 0, 0, 0, 0, 0, 0, 0, 0, 76, 255, 0, 0,
+            216, 4, 23, 11, 79, 101, 0, 0, 0, 0, 208, 208, 28, 21, 76, 255, 0, 0,
         ];
         let mut d = rmp_serde::decode::Deserializer::new(data);
         let result = Datetime::deserialize(&mut d).unwrap();
