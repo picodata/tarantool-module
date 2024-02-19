d548dc44f70474ee7a44b1902da20150063f8c4c
Georgy Moshkin <gmoshkin@picodata.io>
2024-02-19 16:24:53 +0300

    refactor: use tarantool::error::Error in examples/async-h1

--- examples/async-h1-client/src/lib.rs
+++ examples/async-h1-client/src/lib.rs
@@ -1,44 +1,46 @@
 use http_types::{Method, Request, Url};
+use tarantool::error::Error;
 use tarantool::fiber;
 use tarantool::network::client::tcp::TcpStream;
 use tarantool::network::client::tcp::UnsafeSendSyncTcpStream;
 use tarantool::proc;
 
 #[proc]
-fn get(url: &str) -> http_types::Result<()> {
+fn get(url: &str) -> Result<(), Error> {
     fiber::block_on(async {
         println!("Connecting...");
-        let url = Url::parse(url)?;
+        let url = Url::parse(url).map_err(Error::other)?;
         let host = url
             .host_str()
-            .ok_or(http_types::Error::from_display("host not specified"))?;
+            .ok_or_else(|| Error::other("host not specified"))?;
         let req = Request::new(Method::Get, url.clone());
         let mut res = match url.scheme() {
             "http" => {
-                let stream = TcpStream::connect(host, 80)
-                    .await
-                    .map_err(http_types::Error::from_display)?;
+                let stream = TcpStream::connect(host, 80).await.map_err(Error::other)?;
                 let stream = UnsafeSendSyncTcpStream(stream);
                 println!("Sending request over http...");
-                async_h1::connect(stream, req).await?
+                async_h1::connect(stream, req).await.map_err(Error::other)?
             }
             #[cfg(feature = "tls")]
             "https" => {
-                let stream = TcpStream::connect(host, 443)
-                    .await
-                    .map_err(http_types::Error::from_display)?;
+                let stream = TcpStream::connect(host, 443).await.map_err(Error::other)?;
                 let stream = UnsafeSendSyncTcpStream(stream);
-                let stream = async_native_tls::connect(host, stream).await?;
+                let stream = async_native_tls::connect(host, stream)
+                    .await
+                    .map_err(Error::other)?;
                 println!("Sending request over https...");
-                async_h1::connect(stream, req).await?
+                async_h1::connect(stream, req).await.map_err(Error::other)?
             }
             _ => {
-                return Err(http_types::Error::from_display("scheme not supported"));
+                return Err(Error::other("scheme not supported"));
             }
         };
 
         println!("Response Status: {}", res.status());
-        println!("Response Body: {}", res.body_string().await?);
+        println!(
+            "Response Body: {}",
+            res.body_string().await.map_err(Error::other)?
+        );
         Ok(())
     })
 }
