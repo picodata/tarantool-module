763060b2577089014dcec3b7e6f050a43310c412
Georgy Moshkin <gmoshkin@picodata.io>
2024-04-10 19:37:45 +0300

    refactor: cleanup #[tarantool::test] proc macro code

--- tarantool-proc/src/test.rs
+++ tarantool-proc/src/test.rs
@@ -1,12 +1,22 @@
-use crate::imp;
-use proc_macro::TokenStream as TS1;
 use quote::quote;
-use syn::parse_macro_input;
 
-pub fn impl_macro_attribute(attr: TS1, item: TS1) -> TS1 {
-    let fn_item = parse_macro_input!(item as syn::ItemFn);
-    let args = parse_macro_input!(attr as syn::AttributeArgs);
-    let ctx = Context::from_args(args);
+macro_rules! unwrap_or_compile_error {
+    ($expr:expr) => {
+        match $expr {
+            Ok(v) => v,
+            Err(e) => {
+                return e.to_compile_error().into();
+            }
+        }
+    };
+}
+
+pub fn impl_macro_attribute(
+    attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let fn_item = syn::parse_macro_input!(item as syn::ItemFn);
+    let ctx = unwrap_or_compile_error!(Context::from_args(attr.into()));
     let fn_name = &fn_item.sig.ident;
     let test_name = fn_name.to_string();
     let unique_name = format!("TARANTOOL_MODULE_TEST_CASE_{}", test_name.to_uppercase());
@@ -55,43 +65,57 @@ struct Context {
 }
 
 impl Context {
-    fn from_args(args: syn::AttributeArgs) -> Self {
-        let mut tarantool = imp::path_from_ts2(quote! { ::tarantool });
+    fn from_args(tokens: proc_macro2::TokenStream) -> Result<Self, syn::Error> {
+        let mut tarantool = syn::parse_quote! { ::tarantool };
         let mut linkme = None;
         let mut section = None;
         let mut should_panic = false;
 
-        for arg in args {
-            if let Some(path) = imp::parse_lit_str_with_key(&arg, "tarantool") {
-                tarantool = path;
-                continue;
-            }
-            if let Some(path) = imp::parse_lit_str_with_key(&arg, "linkme") {
-                linkme = Some(path);
-                continue;
-            }
-            if let Some(path) = imp::parse_lit_str_with_key(&arg, "section") {
-                section = Some(path);
-                continue;
-            }
-            if imp::is_path_eq_to(&arg, "should_panic") {
-                should_panic = true;
-                continue;
-            }
-            panic!("unsuported attribute argument: {:?}", arg)
-        }
+        syn::parse::Parser::parse2(
+            |input: syn::parse::ParseStream| -> Result<(), syn::Error> {
+                while !input.is_empty() {
+                    let ident: syn::Ident = input.parse()?;
+                    if ident == "tarantool" {
+                        input.parse::<syn::Token![=]>()?;
+                        let value: syn::LitStr = input.parse()?;
+                        tarantool = value.parse()?;
+                    } else if ident == "linkme" {
+                        input.parse::<syn::Token![=]>()?;
+                        let value: syn::LitStr = input.parse()?;
+                        linkme = Some(value.parse()?);
+                    } else if ident == "section" {
+                        input.parse::<syn::Token![=]>()?;
+                        let value: syn::LitStr = input.parse()?;
+                        section = Some(value.parse()?);
+                    } else if ident == "should_panic" {
+                        should_panic = true;
+                    } else {
+                        return Err(syn::Error::new(
+                            ident.span(),
+                            format!("unknown argument `{ident}`, expected one of `tarantool`, `linkme`, `section`, `should_panic`"),
+                        ));
+                    }
 
-        let section = section.unwrap_or_else(|| {
-            imp::path_from_ts2(quote! { #tarantool::test::TARANTOOL_MODULE_TESTS })
-        });
+                    if !input.is_empty() {
+                        input.parse::<syn::Token![,]>()?;
+                    }
+                }
 
-        let linkme = linkme.unwrap_or_else(|| imp::path_from_ts2(quote! { #tarantool::linkme }));
+                Ok(())
+            },
+            tokens,
+        )?;
 
-        Self {
+        let section = section
+            .unwrap_or_else(|| syn::parse_quote! { #tarantool::test::TARANTOOL_MODULE_TESTS });
+
+        let linkme = linkme.unwrap_or_else(|| syn::parse_quote! { #tarantool::linkme });
+
+        Ok(Self {
             tarantool,
             section,
             linkme,
             should_panic,
-        }
+        })
     }
 }
